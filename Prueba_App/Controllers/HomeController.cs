24439730cad2a24e28b081fed2f78ce0dbc62e26
llce05 <llce0@DESKTOP-LQGAPP1>
2017-06-11 20:16:05 -0400

    Cambio

--- Prueba_App/Controllers/HomeController.cs
+++ Prueba_App/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace Prueba_App.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return Content("Esto es una prueba.");
+            return Content("Esto es una prueba 1.");
         }
     }
 }
